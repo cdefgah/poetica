811e53c1a1c0b2b3a07598e2e07ea14b95501436
Rafael Osipov <rafael.osipov@outlook.com>
2020-01-03 15:19:26 +0300

    Added get text and download rest-endpoints

---
+++ src/main/java/com/cdefgah/poetica/controllers/ExportController.java
@@ -0,0 +1,49 @@
+package com.cdefgah.poetica.controllers;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+public class ExportController {
+
+    private static String SERVER_LOCATION = "C:\\Temp\\";
+
+    @RequestMapping("/export/results")
+    public String index() {
+        return "Abartulke Mabulke Tarampulke!";
+    }
+
+    @RequestMapping(path = "/download", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download(@RequestParam("image") String image) throws IOException {
+
+        File file = new File(SERVER_LOCATION + File.separator + image);
+
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
+}
