18b124ffadfd2090b6421cf630be3bef67dd92ce
Rafael Osipov <rafael.osipov@outlook.com>
2020-01-03 11:16:23 +0300

    first commit

---
+++ src/main/java/com/cdefgah/poetica/PoeticaApplication.java
@@ -0,0 +1,13 @@
+package com.cdefgah.poetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdefgah/poetica/PoeticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdefgah.poetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
