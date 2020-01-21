f2dcc4b68f9796794ca6d4f072385a58941e626b
Rafael Osipov <rafael.osipov@outlook.com>
2020-01-21 16:20:02 +0300

    Styling

--- src/main/java/com/cdefgah/poetica/PoeticaApplication.java
+++ src/main/java/com/cdefgah/poetica/PoeticaApplication.java
@@ -1,23 +1,11 @@
 package com.cdefgah.poetica;
 
-import com.cdefgah.poetica.model.Team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PoeticaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PoeticaApplication.class, args);
-
-//		ApplicationContext context = SpringApplication.run(PoeticaApplication.class, args);
-//		TeamsRepository repo = context.getBean(TeamsRepository.class);
-
-//		Team team = new Team();
-//		team.setTeamNumber("ZIZIZI90999");
-//		team.setTeamTitle("STARTupppppo!");
-
-//		repo.save(team);
 	}
 }
