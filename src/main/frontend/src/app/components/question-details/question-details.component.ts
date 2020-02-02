bedcbe5b73fca2438a13e261cdcca00437171792
Rafael Osipov <rafael.osipov@outlook.com>
2020-02-02 17:00:21 +0300

    add buttons ok anc cancel to the dialog

--- src/main/frontend/src/app/components/question-details/question-details.component.ts
+++ src/main/frontend/src/app/components/question-details/question-details.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-question-details',
-  templateUrl: './question-details.component.html',
-  styleUrls: ['./question-details.component.css']
+  selector: "app-question-details",
+  templateUrl: "./question-details.component.html",
+  styleUrls: ["./question-details.component.css"]
 })
 export class QuestionDetailsComponent implements OnInit {
+  dialogAccepted: boolean = false;
 
-  constructor() { }
+  constructor(public dialog: MatDialogRef<QuestionDetailsComponent>) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  acceptDialog() {
+    this.dialogAccepted = true;
+    this.dialog.close();
+    console.log("******************");
+    console.log("ACCEPTED");
+    console.log("******************");
   }
 
+  cancelDialog() {
+    this.dialogAccepted = false;
+    this.dialog.close();
+    console.log("******************");
+    console.log("CANCELED");
+    console.log("******************");
+  }
 }
