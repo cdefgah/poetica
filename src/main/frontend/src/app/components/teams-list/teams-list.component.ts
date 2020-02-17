8721c653aae085f891b716a84ece82ac808b7d6b
Rafael Osipov <rafael.osipov@outlook.com>
2020-02-18 00:16:09 +0300

    Working on teams list

--- src/main/frontend/src/app/components/teams-list/teams-list.component.ts
+++ src/main/frontend/src/app/components/teams-list/teams-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Team } from "src/app/model/Team";
 
 @Component({
-  selector: 'app-teams-list',
-  templateUrl: './teams-list.component.html',
-  styleUrls: ['./teams-list.component.css']
+  selector: "app-teams-list",
+  templateUrl: "./teams-list.component.html",
+  styleUrls: ["./teams-list.component.css"]
 })
 export class TeamsListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  displayedColumns: string[] = ["number", "title"];
 
+  dataSource: Team[];
+
+  onRowClicked(row: any) {}
 }
