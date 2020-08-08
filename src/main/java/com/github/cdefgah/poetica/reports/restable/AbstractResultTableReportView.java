338a478e5047626cfe709dc68324da473c0a9a56
Rafael Osipov <rafael.osipov@outlook.com>
2020-08-09 00:38:17 +0300

    WIP

--- src/main/java/com/github/cdefgah/poetica/reports/restable/AbstractResultTableReportView.java
+++ src/main/java/com/github/cdefgah/poetica/reports/restable/AbstractResultTableReportView.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import java.util.Collections;
 
 public class AbstractResultTableReportView {
 
@@ -13,13 +14,36 @@ public class AbstractResultTableReportView {
     @Autowired
     EntityManager entityManager;
 
+    protected int maxTeamNumberLength;
 
-    protected int getMaxTeamNumberLength() {
-        final TypedQuery<Integer> query =
-                            entityManager.createQuery("select max(team.number) FROM Team team", Integer.class);
-
-        
-
+    public AbstractResultTableReportView() {
+        maxTeamNumberLength = getMaxTeamNumberLength();
     }
 
+    /**
+     * Формирует и отдаёт выровненный по правому краю текст.
+     * @param placeHolderLength длина поля в символах, в котором будет располагаться текст.
+     * @param text текст, который должен быть выровнен по правому краю пробелами.
+     * @return выровненный по правому краю пробелами текст.
+     */
+    protected String getRightAlignedText(int placeHolderLength, String text) {
+        final int lengthDelta = placeHolderLength - text.length();
+        if (lengthDelta > 0) {
+            return String.join("", Collections.nCopies(lengthDelta, " ")) + text;
+        } else {
+            return text;
+        }
+    }
+
+    /**
+     * Возвращает максимальную длину в символах номера команды.
+     * @return максимальная длина в символах номера команды.
+     */
+    private int getMaxTeamNumberLength() {
+        TypedQuery<Integer> query = entityManager.createQuery("select max(team.number) FROM Team team",
+                Integer.class);
+        final Integer maxTeamNumberObject = query.getSingleResult();
+        final int maxTeamNumber = maxTeamNumberObject != null ? maxTeamNumberObject : 0;
+        return String.valueOf(maxTeamNumber).length();
+    }
 }
