1a7de97b581319e0c5041a36435783f245ffc0dd
Rafael Osipov <rafael.osipov@outlook.com>
2020-09-27 19:46:24 +0300

    Started work on tests

--- src/test/java/com/github/cdefgah/poetica/PoeticaApplicationTest.java
+++ src/test/java/com/github/cdefgah/poetica/PoeticaApplicationTest.java
@@ -5,10 +5,18 @@
 
 package com.github.cdefgah.poetica;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class PoeticaApplicationTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void main() {
 
+    }
+
+    @Test
+    public void contextLoads() {
     }
 }
