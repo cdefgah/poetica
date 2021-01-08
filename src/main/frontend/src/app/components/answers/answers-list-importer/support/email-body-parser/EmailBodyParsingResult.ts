4c8957633ecaeb1baf75731921fcc50fe0ad29ae
Rafael Osipov <rafael.osipov@outlook.com>
2021-01-08 14:11:12 +0300

    Update year in the file headers

--- src/main/frontend/src/app/components/answers/answers-list-importer/support/email-body-parser/EmailBodyParsingResult.ts
+++ src/main/frontend/src/app/components/answers/answers-list-importer/support/email-body-parser/EmailBodyParsingResult.ts
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: GPL-3.0-only
- * Copyright (c) 2020 by Rafael Osipov <rafael.osipov@outlook.com>
+ * Copyright (c) 2020 - 2021 by Rafael Osipov <rafael.osipov@outlook.com>
  */
 
 import { AnswerDataModel } from 'src/app/data-model/AnswerDataModel';
