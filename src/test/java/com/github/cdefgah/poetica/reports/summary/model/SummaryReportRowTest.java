ddb54876ebe7e854c90835a65719f1305b86e6d2
Rafael Osipov <rafael.osipov@outlook.com>
2021-01-22 23:03:07 +0300

    chore - added header to the test class

--- src/test/java/com/github/cdefgah/poetica/reports/summary/model/SummaryReportRowTest.java
+++ src/test/java/com/github/cdefgah/poetica/reports/summary/model/SummaryReportRowTest.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * Copyright (c) 2020 - 2021 by Rafael Osipov <rafael.osipov@outlook.com>
+ */
+
 package com.github.cdefgah.poetica.reports.summary.model;
 
 import com.github.cdefgah.poetica.reports.summary.model.SummaryReportModel.SummaryReportRow;
